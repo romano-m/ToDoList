28f1815c150284b11a68a7c2ee6312fe4509fad9
Romain Monclus <romain.monclus@gmail.com>
2014-07-07 22:51:13 +0200

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Romain Monclus on 07/07/2014.
+//  Copyright (c) 2014 Romain Monclus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
